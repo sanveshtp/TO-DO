03d7b04a23d1cea3e5cb960d03c8181e8989978c
sanveshtp <sanveshthumkunta@gmail.com>
2024-02-09 18:58:09 +0530

    Intial

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+// import React, { useState } from "react";
+import React, { useState } from "react";
+import "./App.css";
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  const addTodo = () => {
+    if (newTodo.trim() === "") {
+      return;
+    }
+
+    const newTodoItem: Todo = {
+      id: Date.now(),
+      text: newTodo,
+      completed: false,
+    };
+
+    setTodos((prevTodos) => [...prevTodos, newTodoItem]);
+    setNewTodo("");
+  };
+
+  const toggleTodo = (id: number) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const removeTodo = (id: number) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(todo.id)}
+            />
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => removeTodo(todo.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
